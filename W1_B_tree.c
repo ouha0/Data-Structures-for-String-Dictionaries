3001633f65664bc940ee8fd6e961eeb508c95927
Hai Ou <ouha019@gmail.com>
2024-07-29 16:39:12 +1000

    Working on split child, creating helper functions for more simpler offsetting

--- W1_B_tree.c
+++ W1_B_tree.c
@@ -15,6 +15,7 @@
 #define INITIAL_NODE_USE 0
 
 /* For convenience */
+#define INIT_VAR_SIZE 20
 
 /* Parameter choice */
 #define T_DEGREE 100
@@ -22,10 +23,15 @@
 
 
 /* Function Prototypes */
-char* initialize_empty_node(int, bool);
+char* initialize_node(int, bool, int);
 char* B_tree_create(void);
 char* skip_ptr_block(char**);
 char* get_child(char*, int);
+void read_ptr_block(char*, char*, int);
+char* skip_ptr_block_start(char**, int);
+void move_ptr_block(char*, char*, int); 
+void skip_initialization_var(char**);
+char* init_variable_offset(char*);
 
 
 /* Plan:    
@@ -61,12 +67,12 @@ int main(int argc, char** argv) {
 
 /* Function that builds an empty B-tree (empty root node). */
 char* B_tree_create() {
-    char* root = initialize_empty_node(T_DEGREE, true);
+    char* root = initialize_node(T_DEGREE, true, INITIAL_KEYS);
     return root;
 }
 
 /* Function that creates an emtpy node that takes as input degree of T and boolean is_leaf */
-char* initialize_empty_node(int degree_t, bool is_leaf) {
+char* initialize_node(int degree_t, bool is_leaf, int num_keys) {
 
     /* Allocate memory for node */
     char* node = (char*)malloc(INITIAL_NODE_BYTES * sizeof(char));
@@ -74,7 +80,7 @@ char* initialize_empty_node(int degree_t, bool is_leaf) {
     /* Initialize variables for the node: size = 20 bytes (21 including null-byte) */
 
     *(bool*)node = is_leaf; node += sizeof(bool); // Initialize is_leaf boolean
-    *(int*)(node) = INITIAL_KEYS; node += sizeof(int); // Initialize number of keys
+    *(int*)(node) = num_keys; node += sizeof(int); // Initialize number of keys
     *(int*)(node) = degree_t; node += sizeof(int); // Initialize degree of node 
     *(int*)(node) = INITIAL_NODE_BYTES ; node += sizeof(int); // Initialize node byte size  
     *(int*)(node) = INITIAL_NODE_USE; node += sizeof(int); // Initialize node byte use
@@ -83,17 +89,30 @@ char* initialize_empty_node(int degree_t, bool is_leaf) {
     return node;
 }
 
+/* Function moves a pointer at start of node past the initialization variables to ptr key alternating sequence */
+// For maintainability
+void skip_initialization_var(char** tmp_ptr){
+    (*tmp_ptr) += 20;
+}
+
 /* Function that splits a full node in a B tree. It takes parent node and child node index as input */
+/* Note that node is the parent, child_y is the "left child", child_z is the "right child" */
 char* B_tree_split_child(char* node, int index) {
-    char* offset = node + 20; // 20 is the sizeof(initialization variables)
-    
+    char* offset = node; // 20 is the sizeof(initialization variables)
+    skip_initialization_var(&offset);
+
     /* Search for the ith child (same as read i - 1 keys) */
     char* child_index_address = get_child(node, index);
-    char* child = *(char**)(child_index_address); // A bit uncertain: read as a pointer to a pointer to a char
+    char* child_y = *(char**)(child_index_address); // LHS child 
     
     /* First 4 bytes of child is is_leaf boolean */
-    int sibling_leaf = *(int*)(child);
-    char* new_node = initialize_empty_node(T_DEGREE, sibling_leaf);
+    bool sibling_leaf = *(bool*)(child_y);
+    char* new_child_z = initialize_node(T_DEGREE, sibling_leaf, T_DEGREE - 1); // RHS child
+
+    
+
+    /* Move last (T_degree - 1) keys from child_y to child_z */
+    move_ptr_block(new_child_z, child_y, T_DEGREE - 1); // Move the pointer key sequence from y to z
 
 
     return NULL; // Temporarily here 
@@ -103,7 +122,8 @@ char* B_tree_split_child(char* node, int index) {
 /* Function that gets the index child of the node (skip first i - 1 keys) */
 char* get_child(char* node, int index) {
 
-    char* offset = node + 20; // 20 goes to first pointer block in the node
+    char* offset = node; // 20 goes to first pointer block in the node
+    skip_initialization_var(&offset);
 
     /* Skip the first (index - 1) keys so offset pointer is pointer at node.child_index*/
     for (int i = 0; i < index - 1; i++) {
@@ -126,4 +146,40 @@ char* skip_ptr_block(char** offset_ptr) {
     (*offset_ptr) += tmp_str_length + 1 + sizeof(int); // skip string including null-byte and integer counter 
 
     return NULL;
+}
+
+/* Function that skips the initial variables, and the next "index" number of ptr block sequences. It updates offset to the appropriate address */
+char* skip_ptr_block_start(char** offset_ptr, int index) {
+    skip_initialization_var(offset_ptr); // Skip the initial variables
+    
+    /* Skip the first "index" pointer block sequences */
+    for (int i = 0; i < index; i++) {
+        skip_ptr_block(offset_ptr);
+    }
+    return NULL;
+}
+
+/* Function that moves the first "end" keys from y to z. (It deletes the ptr and blocks accordingly) */
+void move_ptr_block(char* y_child_ptr, char* z_child_ptr, int end) {
+    bool y_is_leaf = *(bool*)(y_child_ptr);
+    char* y_child_cmp;
+    
+    /* Skips first T_DEGREE - 1 keys of y_child_cmp pointer. Moves z_child_cmp pointer to first ptr_key */
+    skip_ptr_block_start(&y_child_cmp, T_DEGREE - 1);
+    char* z_child_cmp = init_variable_offset(z_child_ptr);
+    
+    /* Copy ptr key from y to z, and delete from y */
+    if (!y_is_leaf) {
+
+
+    }
+    else { // Sequence of null pointers and keys
+
+    }
+
+}
+
+/* Function that offsets char* pointer by initial parameters of node. It returns a char* pointer that pointers to ptr key alternating sequence */
+char* init_variable_offset(char* node) {
+    return node += INIT_VAR_SIZE;
 }
