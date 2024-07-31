19dad45bdc93956a0875b94f39c5c5ec494d4629
Hai Ou <ouha019@gmail.com>
2024-07-31 15:12:49 +1000

    Creating new file B trees for managing strings dictionaries

---
+++ B_tree.c
@@ -0,0 +1,22 @@
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/_types/_null.h>
+#include <time.h>
+#include <stdbool.h>
+
+/* Global Variables */
+#define MAX_STRING_BYTES 64
+#define DOUBLE_SIZE 2
+#define INITIAL_COUNT 1
+#define INITIAL_KEYS 0
+#define INITIAL_NODE_USE 0
+
+/* For convenience */
+#define INIT_VAR_SIZE 28 //Initial node maintainence parameter offset
+
+/* Parameter choice */
+#define T_DEGREE 100
+#define NODE_SIZE 200
+
